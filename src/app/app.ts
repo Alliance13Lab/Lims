b4a1bc201de53ca26d437354b72568a7eb37cb7b
abharmal <anavar.bharmal@stacktest.com>
2026-01-07 17:23:07 +0530

    Ng Http loader code added

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,16 @@ import { RouterOutlet } from '@angular/router';
 
 import { setTheme } from 'ngx-bootstrap/utils';
 
+import { NgHttpLoaderComponent, Spinkit } from 'ng-http-loader';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgHttpLoaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
+  protected readonly spinkit = Spinkit;
   protected readonly title = signal('lims');
 
   constructor() {
